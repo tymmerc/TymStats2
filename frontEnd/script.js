002f73da927ddc9dc6339b2031b45311c2c16c0c
MERCIER Tyméo <tymeo.mercier@cub.corsica>
2025-01-23 11:25:41 +0100

    début de collecte de donnée avec js

--- frontEnd/script.js
+++ frontEnd/script.js
@@ -1,6 +1,14 @@
 document.getElementById('spotifyBtn').addEventListener('click', function() {
     alert('Tu vas maintenant afficher tes statistiques Spotify!');
-    // Implémente ici la logique pour récupérer les données Spotify
+    fetch('https:/https://tymmerc.github.io/TymStats2/spotify-data')  // Remplace cette URL par l'URL réelle de ton API
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);  // Vérifie que les données sont bien récupérées
+        // Appelle la fonction pour créer un graphique
+        createChart(data);
+    })
+    .catch(error => console.log('Erreur lors de la récupération des données:', error));
+
 });
 
 document.getElementById('lolBtn').addEventListener('click', function() {
