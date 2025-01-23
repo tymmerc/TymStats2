6e3a22d1f33ee19c79dd5912e25903dfec8c04e0
MERCIER Tyméo <tymeo.mercier@cub.corsica>
2025-01-23 10:43:45 +0100

    changement de la description

---
+++ frontEnd/script.js
@@ -0,0 +1,9 @@
+document.getElementById('spotifyBtn').addEventListener('click', function() {
+    alert('Tu vas maintenant afficher tes statistiques Spotify!');
+    // Implémente ici la logique pour récupérer les données Spotify
+});
+
+document.getElementById('lolBtn').addEventListener('click', function() {
+    alert('Tu vas maintenant afficher tes statistiques League of Legends!');
+    // Implémente ici la logique pour récupérer les données League of Legends
+});
