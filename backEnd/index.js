f23c2246b8c2d58b5685c7bd69dfe41b29d6e9ca
MERCIER Tyméo <tymeo.mercier@cub.corsica>
2025-01-31 13:43:20 +0100

    config of backend with render

---
+++ backEnd/index.js
@@ -0,0 +1,14 @@
+// Charger les variables d'environnement à partir d'un fichier .env
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
